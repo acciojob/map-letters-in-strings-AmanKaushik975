d7c7aa66f0862790de75f813fa015b428f9b55c7
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-05-03 23:50:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+function mapLetters(str) {
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    if (!obj[str[i]]) {
+      obj[str[i]] = [i];
+    } else {
+      obj[str[i]].push(i);
+    }
+  }
+  return obj;
+}
+
+// Do not change the code below
+const str = prompt("Enter str: ");
+alert(JSON.stringify(mapLetters(str)));
